7a94c2a11a6750d42a964fc07ea517801df96f9d
Paulo <monteiro.paulovinicius@gmail.com>
2021-12-17 16:55:31 -0300

    header tweaks

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div className='d-flex'>
-            <div className='m-4'>
+        <div className='d-md-flex'>
+            <div className='mt-4 mb-0 ps-4'>
                 <img src="img/logo.png" alt="logo" height='100'/>
             </div>
 
-            <div className='d-flex align-items-center ms-auto me-4'>
-                <p className='bg-success mt-auto mb-auto mx-2'>Characters</p>
-                <p className='bg-success mt-auto mb-auto mx-2'>Locations</p>
-                <p className='bg-success mt-auto mb-auto mx-2'>Episodes</p>
-                <p className='bg-success mt-auto mb-auto mx-2'>Link3</p>
+            <div className='d-md-flex align-items-center mt-0 text-end ms-auto me-4 navbar-custom'>
+                <p className='mt-auto mb-auto mx-2'>Characters</p>
+                <p className='mt-auto mb-auto mx-2'>Locations</p>
+                <p className='mt-auto mb-auto mx-2'>Episodes</p>
+                <p className='mt-auto mb-auto mx-2'>Link3</p>
             </div>
         </div>
     )
